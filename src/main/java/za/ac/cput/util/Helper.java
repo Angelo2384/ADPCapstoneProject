9da276a1ae752f603ed94bfabceab34fd2514c20
Lyle Solomons <230123872@mycput.ac.za>
2026-03-26 14:40:53 +0200

    Update Helper.java

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -1,11 +1,12 @@
 /* Helper.java
 Helper class
 Author: Lyle Solomons (230123872)
-Date: 25 March 2026
+Date: 26 March 2026
 */
 package za.ac.cput.util;
 
 public class Helper {
+    //Checks whether a given string is null or empty.
     public static boolean isNullOrEmpty(String value) {
         return value == null || value.isEmpty();
     }
